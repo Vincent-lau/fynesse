128f974d3505dcda5e01d481b76ec2ca1bc240f8
Shuntian Liu <shuntian.liu@outlook.com>
2021-11-25 17:20:39 +0000

    yaml version req

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ VERSION = "0.1.0"
 # What packages are required for this module to be executed?
 REQUIRED = [
     "pandas", "numpy", "jupyter", "matplotlib", "pymysql",
-    "yaml>=5.1"
+    "yaml>=6.0"
 ]
 
 # What packages are optional?
